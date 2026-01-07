e46b79a16ea5e954188828f99a3c017d94689052
Jiro Niedeveld <jironiedeveld@coffeeit.nl>
2026-01-07 10:14:57 +0100

    Download Button

--- src/pages/logoSystem.js
+++ src/pages/logoSystem.js
@@ -1,5 +1,6 @@
+import { Query } from "appwrite";
 import { checkAuth } from "../appwrite/auth";
-import { getLogoSystemData } from "../appwrite/db";
+import { getClientId, getCollection, getLogoSystemData } from "../appwrite/db";
 import { getFileDownload, getFilePreview } from "../appwrite/storage";
 import APPWRITE from "../config/public";
 import { renderToast } from "../ui/toast";
@@ -234,3 +235,28 @@ $(".logo-system-variant-showcase-action-wrapper").each((__, elem) => {
     }
   });
 });
+
+let googleDriveURL;
+
+async function gatherGoogleDriveURL() {
+  try {
+    const clientId = await getClientId();
+    const resources = await getCollection(
+      APPWRITE.databases.general.id,
+      APPWRITE.databases.general.collections.resources.id,
+      [Query.equal("client_id", clientId)]
+    );
+    googleDriveURL = resources.documents[0].googledrive_url;
+  } catch (err) {
+    console.error(err);
+    renderToast("Oeps!", getErrorMessage(err), "Negative");
+  }
+}
+
+await gatherGoogleDriveURL();
+
+$("#downloadAllLogos")
+  .off("click.downloadLogos")
+  .on("click.downloadLogos", async function () {
+    window.open(googleDriveURL, "_blank");
+  });
